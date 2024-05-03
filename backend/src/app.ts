d176ae3d1dc95781e0973b5574502c4db630f89b
zaehicks <isaiahhickerson@gmail.com>
2024-05-02 21:26:41 -0700

    added fetch api for census data

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,21 @@
 import express, { Request, Response, Application, NextFunction } from "express";
 import { Server } from "http";
+import censusRouter from "./routes/censusRoutes";
+import cors from "cors";
+
 const app: Application = express();
+
+app.use(cors());
 const PORT = process.env.PORT ?? 4040;
 
 app.get("/", (req: Request, res: Response) => {
   console.log("hi");
 });
 
+app.use(express.json());
+app.use("/api/census", censusRouter);
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-    
-})
+app.use((req: Request, res: Response, next: NextFunction) => {});
 const server: Server = app.listen(4040, () =>
   console.log(`server is on port ${PORT}`)
 );
