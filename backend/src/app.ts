3ef47b527962b6b3352d76a7b987315c6d15b620
Brandon Ramos <bramos005@citymail.cuny.edu>
2024-05-02 23:33:35 -0400

    backend started

---
+++ backend/src/app.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response, Application, NextFunction } from "express";
+import { Server } from "http";
+const app: Application = express();
+const PORT = process.env.PORT ?? 4040;
+
+app.get("/", (req: Request, res: Response) => {
+  console.log("hi");
+});
+
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    
+})
+const server: Server = app.listen(4040, () =>
+  console.log(`server is on port ${PORT}`)
+);
