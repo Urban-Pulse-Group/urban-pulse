ff33a182e7db3378dc31b90044dad83670d087e8
M10mo <mohdkanoongo@gmail.com>
2024-05-06 23:38:21 -0400

    basic styling i have to translate to tailwind next

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './components/home-page/Home';
 import AboutUs from './components/home-page/AboutUs';
 import Mission from './components/home-page/Mission';
 import Map from './components/LeafletMap';
+import './styles.css'
 
 function App() {
   return (
     <Router>
       <NavBar />
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/map">
-          <Map data={{}} />
-        </Route>
-      </Switch>
-      <Home /> {/* im going to render the about us and our mission on the same page*/}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/map" element={<Map data={{}} />} />
+      </Routes>
     </Router>
   );
 }
