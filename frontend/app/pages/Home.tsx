7d0e90b067d75e6cdc5541a26211fd99ff7de3ff
Brandon Ramos <bramos005@citymail.cuny.edu>
2024-05-07 16:03:48 -0400

    merging

--- frontend/app/pages/Home.tsx
+++ frontend/app/pages/Home.tsx
@@ -1,10 +1,19 @@
-import React from "react"
+import React from 'react';
 
-
-export default function Home() {
-    return (
-        <div>
-          
-        </div>
-    )
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Our Website!{/*can figure out what to put here later*, same with the rest of the text */}</h1> 
+      <section>
+        <h2>About Us</h2>
+        <p>...</p>
+      </section>
+      <section>
+        <h2>Mission</h2>
+        <p>...</p>
+      </section>
+    </div>
+  );
 }
+
+export default Home;
---
+++ frontend/app/pages/Mission.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Mission() {
+  return (
+    <div>
+      <h2>Mission</h2>
+      <p>...</p>
+    </div>
+  );
+}
+
+export default Mission;
