50c08a8478d2d27d3a90922fe431d619b56a59da
M10mo <mohdkanoongo@gmail.com>
2024-05-06 23:19:59 -0400

    progress on adding navbar and route to other pages, encoutering dom error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,31 @@
-import { useState } from "react";
-import Map from "./components/LeafletMap";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './components/home-page/Home';
+import AboutUs from './components/home-page/AboutUs';
+import Mission from './components/home-page/Mission';
+import Map from './components/LeafletMap';
 
 function App() {
-  return <Map data={{}} />;
+  return (
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/map">
+          <Map data={{}} />
+        </Route>
+      </Switch>
+      <Home /> {/* im going to render the about us and our mission on the same page*/}
+    </Router>
+  );
 }
 
 export default App;
+// function App() {
+//   return <Map data={{}} />;
+// }
+
+// export default App;
