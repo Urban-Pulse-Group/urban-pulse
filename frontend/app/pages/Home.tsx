c133800cb42381b15d106265726a34e240b41332
M10mo <mohdkanoongo@gmail.com>
2024-05-16 09:56:12 -0400

    fixed home page syntax issue

--- frontend/app/pages/Home.tsx
+++ frontend/app/pages/Home.tsx
@@ -3,7 +3,7 @@ import React from "react";
 // export default function Home() {
 //   return (
 //     <div className="min-h-screen flex flex-col justify-center items-center bg-white">
-//       <h1 className="text-4xl font-bold mb-8">Welcome to Our Website!</h1> 
+//       <h1 className="text-4xl font-bold mb-8">Welcome to Our Website!</h1>
 //       <section className="text-center my-8">
 //         <h2 className="text-2xl font-semibold mb-4">About Us</h2>
 //         <p className="text-lg">...</p>
@@ -28,7 +28,6 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-white">
       <h1 className="text-4xl font-bold mb-8">Welcome to Our Website!</h1>
-
       <nav>
         {/* <ul>
           <li>
@@ -43,10 +42,10 @@ export default function Home() {
         <h2>About Us</h2>
         <p className="text-lg">...</p>
       </section>
-      <section id="mission" className="text-center my-8">
-        <h2>Mission</h2>
-        <p className="text-lg">
-        Moving can be a very difficult and tedious process. Our website aims
+      <section id="mission" className="text-center my-8" />
+      <h2>Mission</h2>
+      <p className="text-lg" />
+      Moving can be a very difficult and tedious process. Our website aims
       <section className="text-center my-8">
         <h2 className="text-2xl font-semibold mb-4">About Us</h2>
         <p className="text-lg">...</p>
@@ -55,7 +54,6 @@ export default function Home() {
         <h2 className="text-2xl font-semibold mb-4">Mission</h2>
         <p className="text-lg">
           Moving can be a very difficult and tedious process. Our website aims
-
           to make this process easier by providing specifics and valuable
           information about the neighborhood. This information includes median
           household income, neighborhood population, and more. It is important
@@ -68,4 +66,4 @@ export default function Home() {
     </div>
   );
 }
-// https://dribbble.com/shots/20618426-Quickit-Figma-web-kit-for-Landings  for inspiration 
+// https://dribbble.com/shots/20618426-Quickit-Figma-web-kit-for-Landings  for inspiration
