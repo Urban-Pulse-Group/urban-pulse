7b1a21993df5ba102a0cba81f4661962b8eb675a
M10mo <mohdkanoongo@gmail.com>
2024-05-15 23:14:49 -0400

    text

--- frontend/app/pages/Home.tsx
+++ frontend/app/pages/Home.tsx
@@ -40,7 +40,7 @@ export default function Home() {
       </nav>
       <section id="about-us" className="text-center my-8">
         <h2>About Us</h2>
-        <p className="text-lg">...</p>
+        <p className="text-lg">We are three software engineering fellows who are passionate about leveraging technology to empower communities. Our website was born out of a shared vision to help people make informed decisions when they move and stay connected with their neighborhoods. We understand the challenges of relocating and the importance of feeling at home in a new area. That's why we created this platform—to provide valuable information and foster a sense of community. We believe that by combining our technical skills with our desire to make a positive impact, we can help individuals and families navigate the moving process with confidence and stay up to date with events and developments in their neighborhoods.</p>
       </section>
       <section id="mission" className="text-center my-8">
         <h2>Mission</h2>
